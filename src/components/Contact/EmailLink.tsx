692b2545eae0a2cc80a379986df01b97319e493f
DavidChen0429 <chenzk0429@gmail.com>
2025-07-22 15:47:06 +0200

    Fix mistakes

--- src/components/Contact/EmailLink.tsx
+++ src/components/Contact/EmailLink.tsx
@@ -50,49 +50,58 @@ interface EmailLinkProps {
   loopMessage?: boolean;
 }
 
-const EmailLink: React.FC<EmailLinkProps> = ({ loopMessage = false }) => {
-  const hold = 50; // ticks to wait after message is complete before rendering next message
-  const delay = 50; // tick length in mS
+// const EmailLink: React.FC<EmailLinkProps> = ({ loopMessage = false }) => {
+//   const hold = 50; // ticks to wait after message is complete before rendering next message
+//   const delay = 50; // tick length in mS
 
-  const [idx, updateIter] = useState(0); // points to current message
-  const [message, updateMessage] = useState(messages[idx]);
-  const [char, updateChar] = useState(0); // points to current char
-  const [isActive, setIsActive] = useState(true); // disable when all messages are printed
+//   const [idx, updateIter] = useState(0); // points to current message
+//   const [message, updateMessage] = useState(messages[idx]);
+//   const [char, updateChar] = useState(0); // points to current char
+//   const [isActive, setIsActive] = useState(true); // disable when all messages are printed
 
-  useInterval(
-    () => {
-      let newIdx = idx;
-      let newChar = char;
-      if (char - hold >= messages[idx].length) {
-        newIdx += 1;
-        newChar = 0;
-      }
-      if (newIdx === messages.length) {
-        if (loopMessage) {
-          updateIter(0);
-          updateChar(0);
-        } else {
-          setIsActive(false);
-        }
-      } else {
-        updateMessage(messages[newIdx].slice(0, newChar));
-        updateIter(newIdx);
-        updateChar(newChar + 1);
-      }
-    },
-    isActive ? delay : null,
-  );
+//   useInterval(
+//     () => {
+//       let newIdx = idx;
+//       let newChar = char;
+//       if (char - hold >= messages[idx].length) {
+//         newIdx += 1;
+//         newChar = 0;
+//       }
+//       if (newIdx === messages.length) {
+//         if (loopMessage) {
+//           updateIter(0);
+//           updateChar(0);
+//         } else {
+//           setIsActive(false);
+//         }
+//       } else {
+//         updateMessage(messages[newIdx].slice(0, newChar));
+//         updateIter(newIdx);
+//         updateChar(newChar + 1);
+//       }
+//     },
+//     isActive ? delay : null,
+//   );
 
+//   return (
+//     <div
+//       className="inline-container"
+//       style={validateText(message) ? {} : { color: 'red' }}
+//       onMouseEnter={() => setIsActive(false)}
+//       onMouseLeave={() => idx < messages.length && setIsActive(true)}
+//     >
+//       <a href={validateText(message) ? `mailto:${message}` : ''}>
+//         <span>{message}</span>
+//       </a>
+//     </div>
+//   );
+// };
+
+const EmailLink: React.FC = () => {
   return (
-    <div
-      className="inline-container"
-      style={validateText(message) ? {} : { color: 'red' }}
-      onMouseEnter={() => setIsActive(false)}
-      onMouseLeave={() => idx < messages.length && setIsActive(true)}
-    >
-      <a href={validateText(message) ? `mailto:${message}` : ''}>
-        <span>{message}</span>
-        <span>@ZekaiChen.com</span>
+    <div className="inline-container">
+      <a href="mailto:chenzk0429@gmail.com">
+        <span>chenzk0429@gmail.com</span>
       </a>
     </div>
   );
