50c1e95b25b524351e52bdb6c6aee1298542bf35
DavidChen0429 <chenzk0429@gmail.com>
2025-07-26 20:53:53 +0200

    Debug

--- src/components/Template/Navigation.tsx
+++ src/components/Template/Navigation.tsx
@@ -1,41 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-
-// import Link from 'next/link';
-
-// import routes from '../../data/routes';
-// import Hamburger from './Hamburger';
-
-// // Websites Navbar, displays routes defined in 'src/data/routes'
-// const Navigation: React.FC = () => (
-//   <header id="header">
-//     <h1 className="index-link">
-//       {routes
-//         .filter((l) => l.index)
-//         .map((l) => (
-//           <Link key={l.label} href={l.path}>
-//             {l.label}
-//           </Link>
-//         ))}
-//     </h1>
-//     <nav className="links">
-//       <ul>
-//         {routes
-//           .filter((l) => !l.index)
-//           .map((l) => (
-//             <li key={l.label}>
-//               <Link href={l.path}>{l.label}</Link>
-//             </li>
-//           ))}
-//       </ul>
-//     </nav>
-//     <Hamburger />
-//   </header>
-// );
-
-// export default Navigation;
-
 'use client';
 
 import React from 'react';
@@ -43,47 +5,10 @@ import React from 'react';
 import Link from 'next/link';
 
 import routes from '../../data/routes';
+import type { Route } from '../../data/routes';
 import Hamburger from './Hamburger';
 
-// 'use client';
-
-// import React from 'react';
-
-// import Link from 'next/link';
-
-// import routes from '../../data/routes';
-// import Hamburger from './Hamburger';
-
-// // Websites Navbar, displays routes defined in 'src/data/routes'
-// const Navigation: React.FC = () => (
-//   <header id="header">
-//     <h1 className="index-link">
-//       {routes
-//         .filter((l) => l.index)
-//         .map((l) => (
-//           <Link key={l.label} href={l.path}>
-//             {l.label}
-//           </Link>
-//         ))}
-//     </h1>
-//     <nav className="links">
-//       <ul>
-//         {routes
-//           .filter((l) => !l.index)
-//           .map((l) => (
-//             <li key={l.label}>
-//               <Link href={l.path}>{l.label}</Link>
-//             </li>
-//           ))}
-//       </ul>
-//     </nav>
-//     <Hamburger />
-//   </header>
-// );
-
-// export default Navigation;
-
-const renderNavLink = (l: any) => {
+const renderNavLink = (l: Route) => {
   if (l.link) {
     // External link
     return (
@@ -117,3 +42,41 @@ const Navigation: React.FC = () => (
 );
 
 export default Navigation;
+
+// 'use client';
+
+// import React from 'react';
+
+// import Link from 'next/link';
+
+// import routes from '../../data/routes';
+// import Hamburger from './Hamburger';
+
+// // Websites Navbar, displays routes defined in 'src/data/routes'
+// const Navigation: React.FC = () => (
+//   <header id="header">
+//     <h1 className="index-link">
+//       {routes
+//         .filter((l) => l.index)
+//         .map((l) => (
+//           <Link key={l.label} href={l.path}>
+//             {l.label}
+//           </Link>
+//         ))}
+//     </h1>
+//     <nav className="links">
+//       <ul>
+//         {routes
+//           .filter((l) => !l.index)
+//           .map((l) => (
+//             <li key={l.label}>
+//               <Link href={l.path}>{l.label}</Link>
+//             </li>
+//           ))}
+//       </ul>
+//     </nav>
+//     <Hamburger />
+//   </header>
+// );
+
+// export default Navigation;
