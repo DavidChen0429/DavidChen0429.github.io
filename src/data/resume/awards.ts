50e60402c94e2e6d7a075c08cb7c146ad5773dfc
DavidChen0429 <chenzk001@outlook.com>
2025-08-04 15:31:03 +0800

    Add awards

--- src/data/resume/awards.ts
+++ src/data/resume/awards.ts
@@ -6,6 +6,7 @@ const awards: Awards[] = [
   { Name: 'President Academic Excellence Initiative PhD Award (UBC) 2026.' },
   { Name: 'Best Startup Business Pitch Presentation (TU Delft, Roland Berger, Yes!Delft) 2024.' },
   { Name: 'Formula Student Germany Racing 2024 Global Ranking 4/79 (FSG, TU Delft) 2024.' },
+  { Name: 'MITACS Globalink Research Internship Scholarship (MIATCS) 2021 (declined).' },
   { Name: 'Outstanding Student Scholarship (BIT) 2018-2022 (x4).' },
   { Name: 'Outstanding Bachelor Student Researcher (BIT) 2020.' },
   {
